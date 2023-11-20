9805fcf6dc3defcbdaeecbd0ff6f4409559c00fe
Cheri Hutchinson <cheribird@msn.com>
2023-11-19 19:34:46 -0600

    Fix last remaining test error of html formatting.

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -30,7 +30,9 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
-    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+    public String displaySearchResults(Model model,
+                                       @RequestParam String searchType,
+                                       @RequestParam String searchTerm) {
 
         ArrayList<Job> jobs;
 
